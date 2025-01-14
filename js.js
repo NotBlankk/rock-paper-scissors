e2ddfe63cb293f86ef6bc606eb5372c061387651
NotBlankk <109392872+NotBlankk@users.noreply.github.com>
2025-01-14 15:40:53 +0530

    Completed Step 3:
    Write the logic to get the human choice

--- js.js
+++ js.js
@@ -1,19 +1,23 @@
-console.log("lol")
 
 let getComputerChoice = () => {
     let val = Math.random()
     if (val > 0.66) {
-        return 'Rock'
+        return 'rock'
 
     } 
     if (val > 0.33 && val <= 0.66) {
-        return 'Paper'
+        return 'paper'
 
     }
     else {
-        return 'Scissors'
+        return 'scissors'
     }
     console.log(val)
 
 }
 console.log(getComputerChoice());
+let getHumanChoice = () => {
+    let val = prompt("Choose Rock Paper Scissors")
+    return val
+}
+console.log(getHumanChoice())

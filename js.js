944003d569c31ec876e5b66cf3b1214915e335a5
NotBlankk <109392872+NotBlankk@users.noreply.github.com>
2025-01-14 15:30:17 +0530

    Completed Step 2: Write the logic to get the computer choice

--- js.js
+++ js.js
@@ -1,1 +1,19 @@
 console.log("lol")
+
+let getComputerChoice = () => {
+    let val = Math.random()
+    if (val > 0.66) {
+        return 'Rock'
+
+    } 
+    if (val > 0.33 && val <= 0.66) {
+        return 'Paper'
+
+    }
+    else {
+        return 'Scissors'
+    }
+    console.log(val)
+
+}
+console.log(getComputerChoice());
